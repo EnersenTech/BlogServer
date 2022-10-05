9c3dfb116298fac69526bb32cd24b0b6df7f49f5
Lewis <lewis@Lewiss-MacBook-Pro.local>
2022-10-05 15:50:01 +0900

    socket connection establish

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const http = require('http')
 const express = require('express')
+const {Server} = require('socket.io')
 const cors = require('cors')
 const path = require('path')
 const fs = require('fs')
@@ -12,19 +13,31 @@ const port = 4000
 
 const server = http.createServer(app)
 
+io = new Server({
+	maxHttpBufferSize: 1e7,
+	pingTimeout: 60000,
+	cors: {
+	  origin: ['http://techenersen.com', 'http://www.techenersen.com', 'http://localhost:3000', 'http://127.0.0.1:3000'],
+	  methods: ['GET', 'POST'],
+	  allowedHeaders: ['my-custom-header'],
+	  credentials: true,
+	},
+	ws: true
+  }).listen(server)
+
 
 app.use('/uploads', express.static(path.join(__dirname + '/src/img/')))
 app.use('/', apiHandler)
-app.use(cors(
-	{
-	origin: ['http://techenersen.com', 'http://www.techenersen.com', 'http://127.0.0.1:3000', 'https://techenersen.com', 'https://techenersen.com', 'http://localhost:3000'],
-	methods: ['GET', 'POST'],
-	allowedHeaders: ['my-custom-header'],
-    credentials: true,
-	}
-))
+app.set('io',io)
+app.use(cors())
 
 
 server.listen(port, () => {
 	console.log('server is running')
 })
+
+
+io.sockets.on('connect', (socket) => {
+	console.log('[' + socket.id + '] connection accepted')}
+)	
+
